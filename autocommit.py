fd9cfdc551427f363ac97f72bda363c618261420
Riju Rahaman <rijurahaman355@gmail.com>
2025-04-17 20:09:44 +0530

    Update autocommit.py

--- autocommit.py
+++ autocommit.py
@@ -34,4 +34,4 @@ def auto_commit():
 # Run the commit function every 10 minutes
 while True:
     auto_commit()
-    time.sleep(600)  # Sleep for 600 seconds (10 minutes)
+    time.sleep(1)  # Sleep for 600 seconds (10 minutes)
